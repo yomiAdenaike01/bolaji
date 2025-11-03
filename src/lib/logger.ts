e2468c488871beb0d76acf642633d68efa658214
Yomi Adenaike <adenaikeyomi@gmail.com>
2025-11-03 15:20:50 +0000

    logger updates

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,12 +1,15 @@
 import pino from "pino";
 
-export const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-    },
-  },
-});
+export const logger =
+  process.env.NODE_ENV === "production"
+    ? pino()
+    : pino({
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+          },
+        },
+      });
