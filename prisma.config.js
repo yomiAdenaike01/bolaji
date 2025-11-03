69cead399de0bf8e3bc8b45c933a61dab02ad94a
Yomi Adenaike <adenaikeyomi@gmail.com>
2025-11-03 14:37:03 +0000

    rename prisma.config.js

---
+++ prisma.config.js
@@ -0,0 +1,14 @@
+const { defineConfig, env } = require('prisma/config');
+require('dotenv').config();
+
+module.exports = defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  engine: 'classic',
+  datasource: {
+    url: env('DATABASE_URL'),
+    directUrl: env('DIRECT_URL'),
+  },
+});
